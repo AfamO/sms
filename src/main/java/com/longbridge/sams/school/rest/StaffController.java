8c7a7f061f3ba48f7f1d8b66355200505002a711
steveoni <stephenoni2@gmail.com>
2018-08-31 12:08:02 +0100

    staff

--- src/main/java/com/longbridge/sams/school/rest/StaffController.java
+++ src/main/java/com/longbridge/sams/school/rest/StaffController.java
@@ -34,58 +34,15 @@ public class StaffController {
 	@Autowired
 	StaffService staffService;
 
-	@PostMapping(consumes="application/json")
-	public ResponseEntity<?>  createOrUpdateStaff(@RequestBody @Valid Staff staff,  Errors errors) {
-		logger.info("staff received is {} " + staff);
 
-		Staff response = null;
-		ResponseEntity<?> resp = null;
-		ResponseData dt = new ResponseData();
-		try {
-			if(errors.hasErrors()) {
-				List<FieldError> err_summary = errors.getFieldErrors().stream().map(f -> new FieldError(f.getField(),f.getDefaultMessage())).collect(Collectors.toList());
-				dt.setError(err_summary);
-				return ResponseEntity.badRequest().body(dt);
-			}
-			if (staff.getId() != null) {
-				Staff staff2 = staffService.getStaff(staff.getId());
-				CustomBeanUtilsBean.getInstance().copyProperties(staff2, staff);
-				staff = staff2;
-				response = staffService.updateStaff(staff);
-				resp = new ResponseEntity<>(new ResponseData (response),HttpStatus.OK);
-			} else {
-				response = staffService.createStaff(staff);
-				resp = new ResponseEntity<>( new ResponseData (response),HttpStatus.OK);
-			}
-
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			logger.debug("Error Adding {}",staff,ex);
-			ResponseData responseData = new ResponseData ();
-			responseData.setError(ex.getMessage());
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseData);
-		}
-		
-		return resp;
-	}
-
-	@GetMapping("{id}/view")
-	ResponseEntity<?> getStaff(@PathVariable Long id) {
-		Staff staff = staffService.getStaff(id);
-		return ResponseEntity.ok(new ResponseData(staff));
-	}
-
-	
 	@GetMapping
 	public DataTablesOutput<Staff> getStaff(DataTablesInput input) {
 		Pageable pageable = DataTablesUtils.getPageable(input);
 
 		Page<Staff> staffers = null;
-//		if (StringUtils.isNoneBlank(search)) {
-//			codes = codeService.findCode(search, pageable);
-//		} else {
+
 		staffers = staffService.getStaff(pageable);
-//		}
+
 		DataTablesOutput<Staff> out = new DataTablesOutput<Staff>();
 		out.setDraw(input.getDraw());
 		out.setData(staffers.getContent());
--- src/main/java/com/longbridge/sams/school/web/StaffView.java
+++ src/main/java/com/longbridge/sams/school/web/StaffView.java
@@ -1,12 +1,19 @@
 package com.longbridge.sams.school.web;
 
+import com.longbridge.sams.model.Staff;
+import com.longbridge.sams.school.service.StaffService;
+import com.longbridge.sams.utils.CustomBeanUtilsBean;
+import com.longbridge.sams.utils.Messages;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -15,15 +22,18 @@ public class StaffView {
 
 	private  final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-  	@GetMapping("/{id}/edit")
-    public String editStaff(@PathVariable Long id, Model model){
-  		model.addAttribute("id", id);
-        return "school/staff/edit";
-    }
+	@Autowired
+    private Messages messages;
+
+	@Autowired
+    private StaffService staffService;
+
+
   	
   	@GetMapping("/new")
-    public String newStaff(){
-        return "school/staff/edit";
+    public String newStaff(Model model){
+        model.addAttribute("staff", new Staff());
+  	    return "school/staff/edit";
     }
   	
     @GetMapping
@@ -32,8 +42,47 @@ public class StaffView {
         return "school/staff/list";
     }
 
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        Staff staff = staffService.getStaff(id);
+        model.addAttribute("staff", staff);
+        return "school/staff/edit";
+    }
+
     @GetMapping("/info")
     public String schoolInfo(){
         return "/school/staff/info";
+    }
+
+    @PostMapping
+    public String createOrUpdateSchool(@ModelAttribute("staff") @Valid Staff staff, BindingResult result,RedirectAttributes redirectAttributes){
+        logger.info("staffDTO received is {} " + staff);
+
+        String response = null;
+
+        try {
+            if (result.hasErrors()) {
+                logger.warn("Error occurred creating Staff{}", result.toString());
+                return "/school/staff/edit";
+            }
+            if (staff.getId() != null) {
+                Staff staff2 = staffService.getStaff(staff.getId());
+                CustomBeanUtilsBean.getInstance().copyProperties(staff2, staff);
+                staff = staff2;
+                staffService.updateStaff(staff);
+                response = "school.update.success";
+
+            } else {
+                staffService.createStaff(staff);
+                response = "staff.create.success";
+            }
+
+        } catch (Exception ex) {
+            result.reject(ex.getMessage());
+            logger.error("Error occurred creating School{}", ex);
+            return "/school/staff/edit";
+        }
+        redirectAttributes.addFlashAttribute("message", messages.get(response));
+        return "redirect:/school/staff";
     }
 }
